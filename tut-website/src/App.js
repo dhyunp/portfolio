679a5a086b530bb2b630887f9345e5233c8345da
dhyunpark <danpark0605@gmail.com>
2020-01-22 17:21:39 +0900

    added basic framework, need to fix DOM issues

--- tut-website/src/App.js
+++ tut-website/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import navBar from './components/navBar'
+import introduction from './components/introduction'
+import about from './components/about'
+import skills from './components/skills'
+import projects from './components/projects'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+	render(){
+		return (
+			<div id="colorlib-page">
+				<div id="colorlib-wrap">
+					<navBar></navBar>
+						<div id="colorlib-main">
+							<introduction></introduction>
+							<about></about>
+							<skills></skills>
+							<projects></projects>
+						</div>
+				</div>
+			</div>
+		);	
+	}
 }
 
 export default App;
